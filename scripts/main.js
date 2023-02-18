4f3b0b80c40a6ade8d9c690e012e2b6b54005b14
leehyukshuai <leehyukshuai@stu.pku.edu.cn>
2023-02-18 14:46:16 +0800

    自定义配色

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ myHeading.textContent = "hello, world!"
 let myImage = document.querySelector('img')
 let f1 = 'images/family.png'
 let f2 = 'images/family-2.png'
-myImage.onclick = function(){
+myImage.onclick = function () {
     let other = f1
     if (myImage.getAttribute('src') === f1) {
         other = f2
@@ -17,7 +17,7 @@ myImage.onclick = function(){
 
 // every click adds an '!'
 let items = document.querySelectorAll('li')
-items.forEach((i)=>{i.addEventListener('click', ()=>{i.textContent += '!'})})
+items.forEach((i) => { i.addEventListener('click', () => { i.textContent += '!' }) })
 
 // add tailored welcome message
 let welcome = document.getElementById('welcome')
@@ -40,3 +40,36 @@ if (!userName) {
 }
 
 document.querySelector('button').onclick = setUserName
+
+// modify the palette
+let form = document.querySelectorAll("input.color")
+
+if (!localStorage.getItem("bgColor")) {
+    localStorage.setItem("bgColor", "#5cadc1")
+    localStorage.setItem("fgColor", "#057d7f")
+    localStorage.setItem("textColor", "#e19889")
+}
+form[0].value = localStorage.getItem("bgColor")
+form[1].value = localStorage.getItem("fgColor")
+form[2].value = localStorage.getItem("textColor")
+
+function resetColor() {
+    document.querySelector('html').style.backgroundColor = form[0].value
+    document.querySelector('body').style.backgroundColor = form[1].value
+    document.querySelector('html').style.color = form[2].value
+    localStorage.setItem("bgColor", form[0].value)
+    localStorage.setItem("fgColor", form[1].value)
+    localStorage.setItem("textColor", form[2].value)
+    alert("颜色应用成功！")
+}
+
+for (let i = 0; i < 3; ++i) {
+    form[i].onchange = resetColor
+}
+
+function initColor() {
+    document.querySelector('html').style.backgroundColor = form[0].value
+    document.querySelector('body').style.backgroundColor = form[1].value
+    document.querySelector('html').style.color = form[2].value
+}
+initColor()
